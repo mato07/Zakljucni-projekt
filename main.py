da255e18ba42b79385830bd28734b632433ce702
Matija Rifl <matija.rifl@gmail.com>
2018-04-22 17:43:39 +0200

    dodana funkcija pogovor in shranjevanje uporabnikov

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import jinja2
 import webapp2
 from google.appengine.api import users
 from models import Sporocilo
+from models import Uporabnik
+from google.appengine.ext import ndb
 
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
@@ -36,6 +38,16 @@ class MainHandler(BaseHandler):
         if user:
             logiran = True
             logout_url = users.create_logout_url('/')
+
+            uporabniki = Uporabnik.query().fetch()
+            emailuporabniki = []
+            for item in uporabniki:
+                if item.email not in emailuporabniki:
+                    emailuporabniki.append(item.email)
+
+            if user.email() not in emailuporabniki:
+                uporabnik = Uporabnik(email=user.email())
+                uporabnik.put()
 
             params = {"logiran": logiran, "logout_url": logout_url, "user": user}
         else:
@@ -79,10 +91,46 @@ class OutboxHandler(BaseHandler):
         params = {"poslana_sporocila": poslana_sporocila, "user": user}
         return self.render_template("outbox.html", params)
 
+class ConversationHandler(BaseHandler):
+    def get(self):
+        user = users.get_current_user()
+        trenutni_uporabnik = user.email()
+        uporabnik_sporocila = Sporocilo.query(ndb.OR(Sporocilo.posilatelj == trenutni_uporabnik,
+                                                     Sporocilo.prejemnik == trenutni_uporabnik)).fetch()
+        uporabniki_pogovor = []
+        for sporocilce in uporabnik_sporocila:
+            if sporocilce.prejemnik not in uporabniki_pogovor and sporocilce.prejemnik!=trenutni_uporabnik:
+                uporabniki_pogovor.append(sporocilce.prejemnik)
+            if sporocilce.posilatelj not in uporabniki_pogovor and sporocilce.posilatelj!=trenutni_uporabnik:
+                uporabniki_pogovor.append(sporocilce.posilatelj)
+        if uporabniki_pogovor != []:
+            relevantni_uporabniki = Uporabnik.query(Uporabnik.email.IN(uporabniki_pogovor)).fetch()
+        else:
+            relevantni_uporabniki = []
+        params = {"user": user, "uporabniki_pogovor": uporabniki_pogovor, "relevantni_uporabniki": relevantni_uporabniki}
+        return self.render_template("conversation.html", params)
+
+class AconversationHandler(BaseHandler):
+    def get(self, uporabnik_id):
+        user = users.get_current_user()
+        trenutni_uporabnik = user.email()
+        uporabnik = Uporabnik.get_by_id(int(uporabnik_id))
+        uporabnik_mail = uporabnik.email
+        poslani_pogovor = Sporocilo.query(ndb.AND(Sporocilo.posilatelj == trenutni_uporabnik,
+                                                     Sporocilo.prejemnik == uporabnik_mail)).fetch()
+        prejeti_pogovor = Sporocilo.query(ndb.AND(Sporocilo.posilatelj == uporabnik_mail,
+                                                     Sporocilo.prejemnik == trenutni_uporabnik)).fetch()
+        params = {"poslani_pogovor": poslani_pogovor, "prejeti_pogovor": prejeti_pogovor, "user": user}
+        return self.render_template("aconversation.html", params)
+
+
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler),
     webapp2.Route('/send_message', SendMessageHandler),
     webapp2.Route('/inbox', InboxHandler),
     webapp2.Route('/result', SendMessageResultHandler),
     webapp2.Route('/outbox', OutboxHandler),
+    webapp2.Route('/conversation', ConversationHandler),
+    webapp2.Route('/conversation/<uporabnik_id:\d+>', AconversationHandler),
+    #webapp2.Route('/conversation/prejemnik@example.com', AconversationHandler),
 ], debug=True)
--- models.py
+++ models.py
@@ -4,3 +4,7 @@ class Sporocilo(ndb.Model):
     besedilo = ndb.StringProperty()
     posilatelj = ndb.StringProperty()
     prejemnik = ndb.StringProperty()
+    nastanek = ndb.DateTimeProperty(auto_now_add=True)
+
+class Uporabnik(ndb.Model):
+    email = ndb.StringProperty()
