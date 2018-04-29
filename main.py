f5c3281d16865443c7a88302b985567983e32cbe
Matija Rifl <matija.rifl@gmail.com>
2018-04-29 15:27:10 +0200

    dodana funkcija pogovor in shranjevanje uporabnikov

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import os
 import jinja2
 import webapp2
-from google.appengine.api import users
+from google.appengine.api import users # uporabljeno za login
 from models import Sporocilo
 from models import Uporabnik
 from google.appengine.ext import ndb
@@ -37,22 +37,22 @@ class MainHandler(BaseHandler):
 
         if user:
             logiran = True
-            logout_url = users.create_logout_url('/')
+            logout_url = users.create_logout_url('/') # ustvari povezavo za logout
 
-            uporabniki = Uporabnik.query().fetch()
+            uporabniki = Uporabnik.query().fetch() # uvozi shranjene uporabnike iz baze
             emailuporabniki = []
             for item in uporabniki:
                 if item.email not in emailuporabniki:
                     emailuporabniki.append(item.email)
 
-            if user.email() not in emailuporabniki:
+            if user.email() not in emailuporabniki: # ce trenutno logiran uporabnik se ni v bazi uporabnikov se ga tja shrani
                 uporabnik = Uporabnik(email=user.email())
                 uporabnik.put()
 
             params = {"logiran": logiran, "logout_url": logout_url, "user": user}
         else:
             logiran = False
-            login_url = users.create_login_url('/')
+            login_url = users.create_login_url('/') # ustvari povezavo za login
 
             params = {"logiran": logiran, "login_url": login_url, "user": user}
 
@@ -62,24 +62,25 @@ class SendMessageHandler(BaseHandler):
     def get(self):
         user = users.get_current_user()
         params = {"user": user}
-        return self.render_template("send_message.html", params)
+        return self.render_template("send_message.html", params) # vrne send message stran
 
 
 class SendMessageResultHandler(BaseHandler):
     def post(self):
-        vneseno_besedilo = self.request.get("vnos-sporocilo")
-        vnesen_prejemnik = self.request.get("vnos-prejemnik")
+        vneseno_besedilo = self.request.get("vnos-sporocilo") # dobi vneseno besedilo
+        vnesen_prejemnik = self.request.get("vnos-prejemnik") # dobi vnesenega prejemnika
         user = users.get_current_user()
         posilatelj = user.email()
         sporocilo = Sporocilo(besedilo=vneseno_besedilo, posilatelj=posilatelj, prejemnik=vnesen_prejemnik)
-        sporocilo.put()
+        sporocilo.put() # spravi sporocilo v bazo
         return self.render_template("result.html")
 
 class InboxHandler(BaseHandler):
     def get(self):
         user = users.get_current_user()
         trenutni_uporabnik = user.email()
-        prejeta_sporocila = Sporocilo.query().filter(Sporocilo.prejemnik == trenutni_uporabnik).fetch()
+        prejeta_sporocila = Sporocilo.query().filter(Sporocilo.prejemnik == trenutni_uporabnik)\
+            .order(Sporocilo.nastanek).fetch() # prikaze prejeta sporocila logiranega uporabnika; order ukaz jih razvrsti po datumu
         params = {"prejeta_sporocila": prejeta_sporocila, "user": user}
         return self.render_template("inbox.html", params)
 
@@ -87,7 +88,8 @@ class OutboxHandler(BaseHandler):
     def get(self):
         user = users.get_current_user()
         trenutni_uporabnik = user.email()
-        poslana_sporocila = Sporocilo.query().filter(Sporocilo.posilatelj == trenutni_uporabnik).fetch()
+        poslana_sporocila = Sporocilo.query().filter(Sporocilo.posilatelj == trenutni_uporabnik)\
+            .order(Sporocilo.nastanek).fetch() # prikaze poslana sporocila logiranega uporabnika; order ukaz jih razvrsti po datumu
         params = {"poslana_sporocila": poslana_sporocila, "user": user}
         return self.render_template("outbox.html", params)
 
@@ -96,15 +98,16 @@ class ConversationHandler(BaseHandler):
         user = users.get_current_user()
         trenutni_uporabnik = user.email()
         uporabnik_sporocila = Sporocilo.query(ndb.OR(Sporocilo.posilatelj == trenutni_uporabnik,
-                                                     Sporocilo.prejemnik == trenutni_uporabnik)).fetch()
-        uporabniki_pogovor = []
+                                                     Sporocilo.prejemnik == trenutni_uporabnik))\
+            .order(Sporocilo.nastanek).fetch() # vrne vsa sporocila v katerih je posilatelj ali prejemnik logirani uporabnik; za ukaz order nisem preprican da ima kaksen pomen tu
+        uporabniki_pogovor = [] # v to listo se shrani uporabnik ki je logiranemu kaj posiljal ali pa od njega prejel sporocila
         for sporocilce in uporabnik_sporocila:
             if sporocilce.prejemnik not in uporabniki_pogovor and sporocilce.prejemnik!=trenutni_uporabnik:
                 uporabniki_pogovor.append(sporocilce.prejemnik)
             if sporocilce.posilatelj not in uporabniki_pogovor and sporocilce.posilatelj!=trenutni_uporabnik:
                 uporabniki_pogovor.append(sporocilce.posilatelj)
-        if uporabniki_pogovor != []:
-            relevantni_uporabniki = Uporabnik.query(Uporabnik.email.IN(uporabniki_pogovor)).fetch()
+        if uporabniki_pogovor != []: # ta if zanka je potrebna ker drugace za uporabnika ki se ni nic posiljal povezava pogovori ne dela
+            relevantni_uporabniki = Uporabnik.query(Uporabnik.email.IN(uporabniki_pogovor)).fetch() # iz baze shranjenih uporabnikov s pomocjo liste uporabniki_pogovor potegnemo te uporabnike
         else:
             relevantni_uporabniki = []
         params = {"user": user, "uporabniki_pogovor": uporabniki_pogovor, "relevantni_uporabniki": relevantni_uporabniki}
@@ -116,11 +119,11 @@ class AconversationHandler(BaseHandler):
         trenutni_uporabnik = user.email()
         uporabnik = Uporabnik.get_by_id(int(uporabnik_id))
         uporabnik_mail = uporabnik.email
-        poslani_pogovor = Sporocilo.query(ndb.AND(Sporocilo.posilatelj == trenutni_uporabnik,
-                                                     Sporocilo.prejemnik == uporabnik_mail)).fetch()
-        prejeti_pogovor = Sporocilo.query(ndb.AND(Sporocilo.posilatelj == uporabnik_mail,
-                                                     Sporocilo.prejemnik == trenutni_uporabnik)).fetch()
-        params = {"poslani_pogovor": poslani_pogovor, "prejeti_pogovor": prejeti_pogovor, "user": user}
+        # pogovor prikaze vsa sporocila v katerih sodelujeta ista uporabnika
+        pogovor = Sporocilo.query(ndb.OR(ndb.AND(Sporocilo.posilatelj == trenutni_uporabnik, Sporocilo.prejemnik == uporabnik_mail),
+                                          ndb.AND(Sporocilo.posilatelj == uporabnik_mail, Sporocilo.prejemnik == trenutni_uporabnik)))\
+            .order(Sporocilo.nastanek).fetch()
+        params = {"pogovor": pogovor, "user": user}
         return self.render_template("aconversation.html", params)
 
 
@@ -132,5 +135,4 @@ app = webapp2.WSGIApplication([
     webapp2.Route('/outbox', OutboxHandler),
     webapp2.Route('/conversation', ConversationHandler),
     webapp2.Route('/conversation/<uporabnik_id:\d+>', AconversationHandler),
-    #webapp2.Route('/conversation/prejemnik@example.com', AconversationHandler),
 ], debug=True)
